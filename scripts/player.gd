abb7875335689b4e0b753bc285e1657390f4b478
Mateus Vinicius Bailich Capistrano <mateus.nn84@gmail.com>
2024-05-25 18:38:02 -0300

    partial commit

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,24 @@ extends CharacterBody2D
 
 @export var speed := Vector2(10, 5);
 
+var shoot_cd := false;
+
 #camera zoom = 0,175
 func _physics_process(delta):
 
 	var input_vector := Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"));
-	
-	global_position += (input_vector * speed)
+	global_position += (input_vector * speed);
 
-	move_and_slide()
+func _process(delta):
+	if Input.is_action_pressed("fire_weapon"):
+		if !shoot_cd:
+			shoot_cd = true
+			shot_laser()
+			await get_tree().create_timer(0.5).timeout
+			shoot_cd = false
+
+func shot_laser():
+	var laser = laser_scene.instantiate()
+	laser.global_position = muzzle.global_position;
+	laser.rotation_degrees = rotation_degrees + 180;
+	emit_signal("laser_shot", laser);
